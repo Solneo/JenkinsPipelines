39fd51433d74c8b069ed96319b5b8916af74d872
Daniil <chdanilr@gmail.com>
2022-06-20 15:42:01 +0300

    temp

--- AndroidTestPipeline.groovy
+++ AndroidTestPipeline.groovy
@@ -1,5 +1,5 @@
 node {
-    withDockerContainer(args: '-u root --privileged', image: "cimg/android:2022.06") {
+    withDockerContainer(args: '-u root --privileged', image: "androidsdk/android-30") {
         stage("Checkout") {
             println("Checkout on dev")
             git url: "https://github.com/Solneo/SpotyTimer.git",
