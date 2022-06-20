fc900d5fc1a2c88aeb4f9a8defdc2b6959215437
Daniil <chdanilr@gmail.com>
2022-06-20 14:56:11 +0300

    temp

--- AndroidTestPipeline.groovy
+++ AndroidTestPipeline.groovy
@@ -1,19 +1,18 @@
 node {
-    agent {
-        docker { image 'node:16.13.1-alpine' }
-    }
-    stage("Checkout") {
-        println("Checkout on dev")
-        git url: "https://github.com/Solneo/SpotyTimer.git",
-                branch: 'master'
+    withDockerContainer(args: '-u root --privileged', image: "cimg/android:2022.06") {
+        stage("Checkout") {
+            println("Checkout on dev")
+            git url: "https://github.com/Solneo/SpotyTimer.git",
+                    branch: 'master'
 
-    }
+        }
 
-    stage("Build") {
-        if (params.BUILD_CONFIG == 'release') {
-            sh './gradlew clean assembleRelease'
-        } else {
-            sh './gradlew clean assembleDebug'
+        stage("Build") {
+            if (params.BUILD_CONFIG == 'release') {
+                sh './gradlew clean assembleRelease'
+            } else {
+                sh './gradlew clean assembleDebug'
+            }
         }
     }
 }
