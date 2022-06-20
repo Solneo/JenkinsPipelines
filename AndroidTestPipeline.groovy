c8658872467bf6ec13414e7982f3ae7cf975cb77
Daniil <chdanilr@gmail.com>
2022-06-20 14:23:00 +0300

    temp

--- AndroidTestPipeline.groovy
+++ AndroidTestPipeline.groovy
@@ -1,18 +1,16 @@
 node {
     stage("Checkout") {
         println("Checkout on dev")
-        git url: "https://github.com/Solneo/Custom_View.git",
-                    branch: 'master'
+        git url: "https://github.com/Solneo/SpotyTimer.git",
+                branch: 'master'
 
     }
 
     stage("Build") {
-        dir("${env.WORKSPACE}/AndroidTest"){
-            if (params.BUILD_CONFIG == 'release') {
-                sh './gradlew clean assembleRelease'
-            } else {
-                sh './gradlew clean assembleDebug'
-            }
+        if (params.BUILD_CONFIG == 'release') {
+            sh './gradlew clean assembleRelease'
+        } else {
+            sh './gradlew clean assembleDebug'
         }
     }
 }
