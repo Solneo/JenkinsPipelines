c68fc0b7f4ffe48d5b7dc6fd4aac38e09b87fa01
daniilcernaev <chdanilr@gmail.com>
2022-06-21 15:15:01 +0300

    temp

--- AndroidTestPipeline.groovy
+++ AndroidTestPipeline.groovy
@@ -2,7 +2,7 @@ node {
     withDockerContainer(args: '-u root --privileged', image: "androidsdk/android-30") {
         stage("Checkout") {
             println("Checkout on dev")
-            git url: "https://github.com/Solneo/SpotyTimer.git",
+            git url: "https://github.com/Solneo/testAppForCi.git",
                     branch: 'master'
 
         }
