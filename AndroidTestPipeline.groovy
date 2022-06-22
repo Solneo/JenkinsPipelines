ec472cae40a8fad1a363669ce8b53178a1083978
daniilcernaev <chdanilr@gmail.com>
2022-06-23 02:18:11 +0300

    temp

--- AndroidTestPipeline.groovy
+++ AndroidTestPipeline.groovy
@@ -1,5 +1,5 @@
 node {
-    withDockerContainer(args: '-u root --privileged', image: "androidsdk/android-30") {
+    withDockerContainer(args: '-u root --privileged', image: "6166cd2795bd") {
         stage("Checkout") {
             println("Checkout on dev")
             git url: "https://github.com/Solneo/testAppForCi.git",
